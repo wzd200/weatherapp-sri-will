618adc1d2fb09dae0dcf1a17e48a1c32c89d9eff
wzd200 <william.z.dye@gmail.com>
2022-02-19 16:07:12 -0500

    added box shadow and top image will restyle

--- weather/src/Components/Conditions/Conditions.js
+++ weather/src/Components/Conditions/Conditions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import clearSky from '../../Images/chris-barbalis-67-CqTBwNI0-unsplash.jpg';
 
 const StyledError = styled.div`
     color: crimson;
@@ -13,8 +14,9 @@ const Conditions = (props) => {
             {props.loading && <div>Loading...</div>}
             {props.responseObj.cod === 200 ?
                 <div className='card'>
-                    <h3 className='card-title'><strong>{props.responseObj.name}</strong></h3>
-                    <p className='card-text'>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
+                    <img src={clearSky} class='card-img-top' alt='clear sky'></img>
+                    <h3 className='card-title py-3'><strong>{props.responseObj.name}</strong></h3>
+                    <p className='card-text mb-2'>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
                 </div>
                 : null
             }
